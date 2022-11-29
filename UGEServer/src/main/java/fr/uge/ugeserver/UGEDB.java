30f567c1d581f219bc376fd39b623748e02d3eb7
Jayque17 <julien.leon99@gmail.com>
2022-11-29 19:25:05 +0100

    RMI and REST done for IUGEDB

--- UGEServer/src/main/java/fr/uge/ugeserver/UGEDB.java
+++ UGEServer/src/main/java/fr/uge/ugeserver/UGEDB.java
@@ -5,14 +5,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 
 @RestController
 @RequestMapping("/uge")
 public class UGEDB extends UnicastRemoteObject implements IUGEDB {
 
+    Logger logger = Logger.getLogger(UGEDB.class.getName());
 
     record UGEUser(String user, String password) {
         public boolean isUser(String name) {
@@ -24,6 +27,7 @@ public class UGEDB extends UnicastRemoteObject implements IUGEDB {
     }
 
     private final HashMap<Long, UGEUser> users = new HashMap<>();
+    private final HashMap<UGEUser, String> connectedUsers = new HashMap<>();
 
 
     public UGEDB() throws RemoteException {
@@ -39,38 +43,51 @@ public class UGEDB extends UnicastRemoteObject implements IUGEDB {
         for (int i = 0; i < 20; i++) {
             users.put((long) i, new UGEUser(name[i], "pwd"));
         }
+        logger.info(users.toString());
     }
 
+    @Override
+    public boolean isTokenValid(String token) {
+        for (String t : connectedUsers.values()) {
+            logger.info("token is " + token);
+            logger.info("t is " + t);
+            if(t.equals(token)) {
+                logger.info("success : token is valid");
+                return true;
+            }
+        }
+        logger.info("failure : token doesn't exist");
+        return false;
+    }
+
+    // code get from https://stackoverflow.com/questions/13992972/how-to-create-a-authentication-token-using-java
+    private String randomToken() {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[16];
+        random.nextBytes(bytes);
+        return bytes.toString();
+    }
 
     @PostMapping(value = "/login")
-    public boolean login(@RequestBody UGEUser ugeUser) throws RemoteException {
+    public String login(@RequestBody UGEUser ugeUser) throws RemoteException {
         for(Map.Entry<Long, UGEUser> user : users.entrySet()) {
             if(user.getValue().isUser(ugeUser.user()) && user.getValue().isPassword(ugeUser.password())) {
-                return true;
+                var token = randomToken();
+                connectedUsers.put(ugeUser, token);
+                logger.info("login success token is " + token);
+                return token;
             }
         }
-        return false;
+
+        logger.info("login has failed");
+        return null;
+    }
+
+    @PostMapping(value = "/logout")
+    public void logout(@RequestBody UGEUser ugeUser) throws RemoteException {
+        var token = connectedUsers.remove(ugeUser);
+        logger.info("logout previous token were " + token);
     }
 
 
-    @Override
-    public boolean login(String name, String password) throws RemoteException {
-        for(Map.Entry<Long, UGEUser> user : users.entrySet()) {
-            if(user.getValue().isUser(name) && user.getValue().isPassword(password)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @GetMapping(value = "/user/{name}")
-    @Override
-    public long getId(@PathVariable String name) throws RemoteException {
-        for(Map.Entry<Long, UGEUser> user : users.entrySet()) {
-            if(user.getValue().isUser(name)) {
-                return user.getKey();
-            }
-        }
-        return -1L;
-    }
 }
